de5660c92cd09e32f1b9f12a186b74bdee1485d2
Prunel Mel <seyive.awaya@gmail.com>
2025-10-13 17:21:05 +0100

    fixing bugs

--- java/ArrayManip.java
+++ java/ArrayManip.java
@@ -1,20 +1,41 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 
 class ArrayManip{
 
-    public static int Result(int n, int [][] queries){
+    public static long Result(int n, List<List<Integer>> queries){
         
-        int [] arr = new int[n];//all values are 0 by default
+        long [] arr = new long[n];//all values are 0 by default
 
-        for(int [] query : queries){
 
-            for(int i=0; i <= query[1] && i >= query[0]; i++){
+        for(List<Integer> query : queries){
 
+            System.out.println(query);
+
+            for(int i=query.get(0); i <= query.get(1) && i >= query.get(0); i++){
+
+                // System.out.println(query);
                 
+                arr[i - 1] += query.get(2);
+
             }
+
+            System.out.println(Arrays.toString(arr));
         }
+
+        return Arrays.stream(arr).max().getAsLong();
    
+    }
+
+
+    public static void main (String [] args){
+
+        List<List<Integer>> queries = List.of(List.of(1,5,3), List.of(4,8,7), List.of(6,9,1));
+
+        long res = Result(10, queries);
+
+        System.err.println(res);
     }
 }
 
